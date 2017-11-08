bf6d504f3b8d643140f7f5cf4455fa9c212f1470
lecroc <clecrone@compasspoint-media.com>
2017-11-08 16:58:00 -0600

    work from 11-8-17

--- project2.R
+++ project2.R
@@ -6,11 +6,13 @@ d1<-mtcars
 d1$Model<-row.names(d1)
 
 plot_ly(d1, x=~d1$mpg, y=~d1$disp, z=~d1$hp, type="scatter3d", color=d1$qsec,
-        mode="markers", text= ~paste(d1$Model, "Quarter Mile:", d1$qsec, sep="<br>")) %>%
+        mode="markers", text= ~paste(d1$Model, "Horsepower:", d1$hp, "Displacement:", d1$disp,
+                                     "MPG:", d1$mpg, "Quarter Mile:", d1$qsec, sep="<br>")) %>%
   layout(
     title = "Horsepower, Displacement, and Mileage for MT Cars Data",
 scene = list(
   xaxis = list(title = "MPG"),
   yaxis = list(title = "Displacement"),
-  zaxis = list(title = "Horsepower")
+  zaxis = list(title = "Horsepower"),
+  hoverinfo=text
 ))
