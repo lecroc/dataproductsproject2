d23393ccc863421e0a1477db98cba72b93226032
lecroc <clecrone@compasspoint-media.com>
2017-11-08 20:09:30 -0600

    Final commit 11-8-17

--- project2.R
+++ project2.R
@@ -5,14 +5,16 @@ library(plotly)
 d1<-mtcars
 d1$Model<-row.names(d1)
 
-plot_ly(d1, x=~d1$mpg, y=~d1$disp, z=~d1$hp, type="scatter3d", color=d1$qsec,
-        mode="markers", text= ~paste(d1$Model, "Horsepower:", d1$hp, "Displacement:", d1$disp,
-                                     "MPG:", d1$mpg, "Quarter Mile:", d1$qsec, sep="<br>")) %>%
+plot<-plot_ly(d1, x=~d1$mpg, y=~d1$disp, z=~d1$hp, type="scatter3d", color=d1$qsec,
+              mode="markers", text= ~paste(d1$Model, "Horsepower:", d1$hp, "Displacement:",
+                                           d1$disp,"MPG:", d1$mpg, "Quarter Mile:", d1$qsec, sep="<br>"),hoverinfo="text") %>%
   layout(
     title = "Horsepower, Displacement, and Mileage for MT Cars Data",
-scene = list(
-  xaxis = list(title = "MPG"),
-  yaxis = list(title = "Displacement"),
-  zaxis = list(title = "Horsepower"),
-  hoverinfo=text
-))
+    scene = list(
+      xaxis = list(title = "MPG"),
+      yaxis = list(title = "Displacement"),
+      zaxis = list(title = "Horsepower")
+    ))%>%
+  config(displayModeBar=F)
+
+plot
